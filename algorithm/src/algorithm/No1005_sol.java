b06b26e1f13a1bbf035f86af066e923a5902517d
Seungyub <iiwvii@naver.com>
2021-07-15 21:51:54 +0900

    2021-07-15 백준 풀이 추가

--- algorithm/src/algorithm/No1005_sol.java
+++ algorithm/src/algorithm/No1005_sol.java
@@ -2,14 +2,13 @@ package algorithm;
 
 import java.util.*;
 import java.io.*;
-// 시간 초과 해결 불가
-public class No1005 {
+// 시간 초과
+public class No1005_sol {
 
-	static int T, N;
+	static int T;
 	static Connect[] con;
 	static int[] time;
-	static int[][] cache;
-
+	static int[] cache;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -18,26 +17,22 @@ public class No1005 {
 
 		for (int i = 0; i < T; i++) {
 			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
 			int K = Integer.parseInt(st.nextToken());
 			time = new int[N + 1];
 
-			cache = new int[N + 1][N+1];
-			for(int a = 0; a < N+1; a++) {
-				for(int b =0; b < N+1; b++) {
-					cache[a][b] = -1;
-				}
-			}
-
+			boolean[] v = new boolean[N+1];
+			cache = new int[N+1];
+			
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= N; j++) {
 				time[j] = Integer.parseInt(st.nextToken());
 			}
-			con = new Connect[N + 1];
-			for (int k = 1; k <= N; k++) {
+			con = new Connect[N+1];
+			for(int k = 1; k <= N; k++) {
 				con[k] = new Connect(k);
 			}
-
+			
 			for (int j = 1; j <= K; j++) {
 				st = new StringTokenizer(br.readLine());
 				int x = Integer.parseInt(st.nextToken());
@@ -46,44 +41,47 @@ public class No1005 {
 			}
 
 			int victory = Integer.parseInt(br.readLine());
-
-			System.out.println(rec(victory,0));
+			
+			bw.write(rec(con[victory], v)+"\n");
 		}
 		bw.flush();
 		bw.close();
 	}
-
-	static class Connect {
+	static class Connect{
 		int num;
 		ArrayList<Integer> list;
-
 		public Connect(int num) {
 			this.num = num;
 			list = new ArrayList<>();
 		}
 	}
-
-	static int rec(int key, int degree) {
-		
-		if(cache[key][degree] != -1) {	// 최대값이 0인 경우가 있으므로 cache를 -1로 초기화해야 한다.
-			return cache[key][degree];
-		}
-		
-		if(con[key].list.size() == 0) {	// list의 size가 0인 경우 값을 반환
-			return time[key];
-		}
+	
+	static int rec(Connect key, boolean[] v) {
+		v[key.num] = true;
 		int max = 0;
 		
-		for(int i : con[key].list) {
-			if(max < rec(i,degree+1)) {
-				max = rec(i,degree+1);
-			}
+		if(cache[key.num] != 0) {
+			return cache[key.num];
 		}
 		
-		max += time[key];	// 구한 max 값에 자신의 값을 더해서 반환
-		cache[key][degree] = max;	// cache 저장
+		
+		if(key.list.size() == 0) {
+			return time[key.num];
+		}
+		
+		for(int i : key.list) {
+			v[i] = true;
+			int temp = rec(con[i],v);
+			if(cache[i] == 0) {
+				cache[i] = temp;
+			}
+			max = max > temp ? max : temp;
+			v[i] = false;
+		}
+		
+		max += time[key.num];
+		
 		return max;
 	}
-	
 
 }
